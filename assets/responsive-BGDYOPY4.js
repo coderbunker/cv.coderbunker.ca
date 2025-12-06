d6d2fb18e668973e93598674eac5f979202a4c6e
Ricky Ng-Adam <rngadam@gmail.com>
2025-12-06 11:14:17 -0500

    feat: build 2025-12-06

--- assets/responsive-BGDYOPY4.js
+++
@@ -1,1 +0,0 @@
-import{r as o,c as t}from"./vendor-BrdkcOQB.js";const e=o("right"),n=t(()=>window.innerWidth<600);function i(){e.value=n?"top":"right"}window.addEventListener("resize",i);i();export{e as l};
---
+++ assets/responsive-CKXmZbnl.js
@@ -0,0 +1,1 @@
+import{r as o,c as t}from"./vendor-CiY54AWt.js";const e=o("right"),n=t(()=>window.innerWidth<600);function i(){e.value=n?"top":"right"}window.addEventListener("resize",i);i();export{e as l};
